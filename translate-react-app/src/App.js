ea64f83419594e5876277a3df2f9cc3d5fd1d9ef
peppe1994 <giuseppecuzzola94@gmail.com>
2020-08-02 11:31:27 +0200

    add material ui and generate target function

--- translate-react-app/src/App.js
+++ translate-react-app/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style/main.scss';
 import TranslateContainer from './component/TranslateContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>Enter a word to translate it into different languages!</h2>
         <TranslateContainer />
-      </header>
     </div>
   );
 }
--- translate-react-app/src/component/TranslateContainer.jsx
+++ translate-react-app/src/component/TranslateContainer.jsx
@@ -1,23 +1,64 @@
 import React, { useState } from 'react';
-import {Validation} from './util/validation';
+import { Validation } from './util/validation';
+import TextField from '@material-ui/core/TextField';
+import { Button, List, ListItem, ListItemText } from '@material-ui/core';
+
 
 const TranslateContainer = (props) => {
     //hooks definition
-    const [source, setSource] = useState({value:'', type:'only-letters'});
+    const [source, setSource] = useState({ value: '', type: 'only-letters' });
+    const [target, setTarget] = useState('');
 
-    
     const onChangeHandler = event => {
-        console.log(Validation(event.target.value, source.type));
-        setSource({value:event.target.value, type:'only-letters'});
-      };
+        if (Validation(event.target.value, source.type))
+            setSource({ value: event.target.value, type: 'only-letters' });
+    };
+
+    const onClickButton = () => {
+        console.log('Button clicked');
+    }
+
+    const generateResult = (element) => {
+        if (target !== null && target !== '') {
+            return target.map((value) => {
+                React.cloneElement(element, {
+                    key: value,
+                })
+            })
+        }
+    }
 
     return <>
-          <input
-            type="text"
-            name="name"
-            onChange={onChangeHandler}
-            value={source.value}
-      />
+        <div className='translate-container'>
+            <TextField
+                name='source'
+                id="standard-basic"
+                label="Word"
+                value={source.value}
+                onChange={onChangeHandler}
+            />
+            <br></br>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={onClickButton}
+                type='button'
+            >
+                TRANSLATE
+        </Button>
+            <br></br><br></br>
+            <p>Result:</p>
+            <List>
+                {generateResult(
+                <ListItem>
+                    <ListItemText
+                        primary='Single-line item'
+                        secondary='Secondary text'
+                    />
+                </ListItem>
+                )}
+            </List>
+        </div>
     </>
 }
 
